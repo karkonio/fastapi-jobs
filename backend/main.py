348ccb56984c36f963b4baabaa15f9534800ebf6
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-05-04 12:43:57 +0600

    add postgres connection

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from backend.core.config import settings
 from backend.apis.general_pages.route_homepage import general_pages_router
+from backend.db.session import engine
+from backend.db.base_class import Base
 
 BASE_PATH = Path(__file__).resolve().parent
 
@@ -20,13 +22,23 @@ def configure_static(app):  #new
 	)
 
 
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+
 def start_application():
+	print(f'------THE settings : {settings.POSTGRES_USER} and {settings.POSTGRES_PASSWORD}')
+	print(f'------THE settings : {settings.POSTGRES_SERVER} and {settings.POSTGRES_PORT}')
+	print(f'------THE settings : {settings.POSTGRES_DB} and {settings.DATABASE_URL}')
 	app = FastAPI(
         title=settings.PROJECT_NAME,
         version=settings.PROJECT_VERSION
     )
+
+
 	include_router(app)
 	configure_static(app)
+	create_tables()
 	return app
 
 
