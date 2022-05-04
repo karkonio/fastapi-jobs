8a8a7d5947e31ace26a3c897f14e59331c510459
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-05-04 17:36:41 +0600

    add job_create route + test

---
+++ tests/test_routes/test_jobs.py
@@ -0,0 +1,16 @@
+import json
+
+
+def test_create_job(client):
+    data = {
+        "title": "Python developer",
+        "company": "DataArt",
+        "company_url": "dataart.kz",
+        "location": "Kazakhstan, Almaty",
+        "description": "python",
+        "date_posted": "2022-04-04"
+        }
+    response = client.post("/jobs/create-job/", json.dumps(data))
+    assert response.status_code == 200
+    assert response.json()["company"] == "DataArt"
+    assert response.json()["description"] == "python"
