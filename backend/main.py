a86a2ba2dc7b3965b5b7cb4a57ab54e5dc1b9b6f
karina.yesbukenova <karina.yesbukenova@gmail.com>
2022-05-04 11:12:34 +0600

    add serving static files

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from backend.core.config import settings
 from backend.apis.general_pages.route_homepage import general_pages_router
+
+BASE_PATH = Path(__file__).resolve().parent
 
 
 def include_router(app):
 	app.include_router(general_pages_router)
+
+
+def configure_static(app):  #new
+    app.mount(
+		"/static",
+		StaticFiles(directory=str(BASE_PATH / "static")),
+		name="static"
+	)
 
 
 def start_application():
@@ -13,6 +26,7 @@ def start_application():
         version=settings.PROJECT_VERSION
     )
 	include_router(app)
+	configure_static(app)
 	return app
 
 
